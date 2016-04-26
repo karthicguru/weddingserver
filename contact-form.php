756d48050480a16ffb4f09496c6f274a93b29356
karthicguru <karthick234@Gmail.com>
2016-04-26 16:38:47 +0530

    server file for rsvp

---
+++ contact-form.php
@@ -0,0 +1,22 @@
+<?php
+  $name = $_POST['name'];
+  $count = $_POST['guests'];
+  $event = $_POST['attending'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+   $email_from = 'RSVP@babuwedskarthiga.com';
+ 
+    $email_subject = "New RSVP submission";
+ 
+    $email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+
+     $to = "karthick234@gmail.com";
+ 
+  $headers = "From: $email_from \r\n";
+ 
+  $headers .= "Reply-To: $visitor_email \r\n";
+ 
+  mail($to,$email_subject,$email_body,$headers);
+?>
